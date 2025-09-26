1291dc7a132ab206d762117440b4b7305cf09808
madotto.Axel@outlook.com <madotto.axel@outlook.com>
2025-09-26 11:01:38 +0200

    change about phone position

--- src/components/client/About.tsx
+++ src/components/client/About.tsx
@@ -8,10 +8,10 @@ export const About = () => {
     const { t } = useT("about");
 
     return (
-        <div className="sm:flex flex-col p-3 min-h-screen pt-20 items-center justify-center">
+        <div className="flex flex-col p-3 min-h-screen pt-20 items-center justify-center">
             <h2>{t("presentation")}</h2>
 
-            <div className="flex p-4 sm:p-10">
+            <div className="sm:flex p-4 sm:p-10">
                 <Image
                     src={profile}
                     width={100}
@@ -19,7 +19,7 @@ export const About = () => {
                     className="object-contain"
                     alt="Picture of the author"
                 />
-                <p className="p-10">{t("paragraphe-about")}</p>
+                <p className="p-3 sm:p-20">{t("paragraphe-about")}</p>
             </div>
         </div>
     );
