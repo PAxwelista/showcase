159854d7869e4df93d069dbb27ead0c1e06110ac
madotto.Axel@outlook.com <madotto.axel@outlook.com>
2025-09-30 11:36:10 +0200

    correct Btn componant => className

--- src/components/client/Btn.tsx
+++ src/components/client/Btn.tsx
@@ -5,7 +5,7 @@ export const Btn = (props: Props) => {
     return (
         <button
             className={
-                `text-white px-2 cursor-pointer mx-2 h-8 bg-blue-500 rounded-full duration-200 active:scale-95 hover:scale-110` +
+                `text-white px-2 cursor-pointer mx-2 h-8 bg-blue-500 rounded-full duration-200 active:scale-95 hover:scale-110 ` +
                 className
             }
             {...otherProps}
