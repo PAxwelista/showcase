4aa460495b401e46bc28ecaa64d886be715eb7be
madotto.Axel@outlook.com <madotto.axel@outlook.com>
2025-09-30 10:15:02 +0200

    change font family, text size and picture center of about

--- src/components/client/About.tsx
+++ src/components/client/About.tsx
@@ -12,15 +12,17 @@ export const About = () => {
         <div className="flex flex-col p-3 min-h-screen pt-20 items-center justify-center">
             <h2>{t("presentation")}</h2>
 
-            <div className="sm:flex p-4 sm:p-10">
-                <Image
-                    src={profile}
-                    width={100}
-                    height={100}
-                    className="object-contain"
-                    alt="Picture of the author"
-                />
-                <p className="p-3 sm:p-20">{t("paragraphe-about")}</p>
+            <div className="sm:flex p-4 sm:p-10 items-center">
+                <div className="h-50 w-50 rounded-full overflow-hidden" >
+                    <Image
+                        src={profile}
+                        width={300}
+                        height={300}
+                        className="relative left-4 bottom-2 scale-120"
+                        alt="Picture of the author"
+                    />
+                </div>
+                <p className="flex-3 p-3 sm:p-20 whitespace-pre-line">{t("paragraphe-about")}</p>
             </div>
             {/* <TargetBlur/> */}
         </div>
